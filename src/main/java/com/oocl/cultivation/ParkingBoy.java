75067c8e9e8393105a9c4041cc85f0a01fc3ab09
Devin <991476256@qq.com>
2020-07-28 13:55:48 +0800

    [Parking story 6][devin] refactor: reformat foreach

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -22,13 +22,13 @@ public class ParkingBoy implements ParkingAble {
     @Override
     public CarTicket park(Car car) throws NoParkingSpaceException {
         CarTicket carTicket;
-        for (ParkingLot parkingLot : parkingLots
-        ) {
+        for (ParkingLot parkingLot : parkingLots) {
             if (parkingLot.checkSurplusCapacity() != 0) {
                 carTicket = parkingLot.carIn(car);
                 return carTicket;
             }
         }
+        //todo extract constant string
         throw new NoParkingSpaceException("Not enough position.");
     }
 
